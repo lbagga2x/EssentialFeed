498b32b12c86f582df62b274938dba98a8208ab9
Lalit Bagga <l.bagga@combyne.ag>
2023-01-26 17:28:51 -0500

    Create FeedItem Modal

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Lalit Bagga on 2023-01-26.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
