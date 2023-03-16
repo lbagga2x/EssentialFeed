89876bd73d1112fab930c3699b67759d3b56600b
Lalit Bagga <l.bagga@combyne.ag>
2023-03-16 18:59:52 -0400

    Move LocalFeedItem to own file

--- EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -14,18 +14,3 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?)-> Void
     func insert(_ feedItems: [LocalFeedItem], _ timestamp: Date, completion: @escaping InsertionCompletion)
 }
-
-public struct LocalFeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-    
-}
---
+++ EssentialFeed/FeedCache/LocalFeedItem.swift
@@ -0,0 +1,23 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Lalit Bagga on 2023-03-16.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+    
+}
