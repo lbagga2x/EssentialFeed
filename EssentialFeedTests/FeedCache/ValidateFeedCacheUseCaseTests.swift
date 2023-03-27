091e93ae158d3f7e6cf2fe44caad2c739c7f3b21
Lalit Bagga <l.bagga@combyne.ag>
2023-03-27 18:16:50 -0400

    ocalFeedLoader does not message store upon creation (before validati… …ng the cached feed)

---
+++ EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Lalit Bagga on 2023-03-27.
+//
+
+import XCTest
+import EssentialFeed
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMesseges, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+}
